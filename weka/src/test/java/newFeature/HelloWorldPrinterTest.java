42b3d1d21bbb0b5e5fa233cd76029a2b4ec81654
Unknown <aymenamous@gmail.com>
2019-02-21 19:58:18 -0500

    Test that does not work

--- weka/src/test/java/newFeature/HelloWorldPrinterTest.java
+++ weka/src/test/java/newFeature/HelloWorldPrinterTest.java
@@ -1,15 +1,16 @@
 package newFeature;
 
-import org.junit.Test;
+
 
 import junit.framework.Assert;
 import junit.framework.TestCase;
 
-public class HelloWorldPrinterTest {
+public class HelloWorldPrinterTest extends TestCase {
+	private HelloWolrdPrinter printer = new HelloWolrdPrinter();
+	  
 	
-	@Test
-	public void getTextReturnsHelloWorld() {
-		Assert.assertEquals("Hello world", new HelloWolrdPrinter().getText());
+	public void testgetText() {
+	    assertEquals("Hello world", printer.getText());
 	}
 	
 }
